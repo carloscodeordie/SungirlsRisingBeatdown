e2325548ed683504d72e10ea34e65b887b21f623
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-01-28 13:43:55 -0600

    Allows the player to Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,25 +3,63 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Config parameters
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
+    // States variables
+
+    // Cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
+        FlipSprite();
     }
 
     private void Run() {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        myAnimator.SetBool("isRunning", isPlayerRunning());
+    }
+
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity = myRigidBody.velocity + jumpVelocity;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        if (isPlayerRunning()) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x) * 2f, 2f);
+        }
+    }
+
+    private bool isPlayerRunning()
+    {
+        return Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
     }
 }
