dcce71004d3ec9ac5aa13b31c73d1470f07744ed
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-02-05 17:08:44 -0600

    Allows the player to climb ladders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
     // Config parameters
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // States variables
 
@@ -13,6 +14,7 @@ public class Player : MonoBehaviour
     Rigidbody2D myRigidBody;
     Animator myAnimator;
     Collider2D myCollider2D;
+    float gravityScaleAtStart;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class Player : MonoBehaviour
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
     // Update is called once per frame
@@ -28,6 +31,7 @@ public class Player : MonoBehaviour
         Run();
         Jump();
         FlipSprite();
+        ClimbLadder();
     }
 
     private void Run() {
@@ -35,6 +39,22 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
         myAnimator.SetBool("isRunning", isPlayerRunning());
+    }
+
+    private void ClimbLadder()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("isClimbing", false);
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+        myAnimator.SetBool("isClimbing", isPlayerClimbing());
+        myRigidBody.gravityScale = 0f;
     }
 
     private void Jump()
@@ -61,5 +81,10 @@ public class Player : MonoBehaviour
     private bool isPlayerRunning()
     {
         return Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private bool isPlayerClimbing()
+    {
+        return Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
     }
 }
