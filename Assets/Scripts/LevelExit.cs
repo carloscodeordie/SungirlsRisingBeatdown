a89040fad9a0c9e908d94e27b363d9f50eae3003
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-02-14 11:19:46 -0600

    Add Exit Levels objects

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float levelSlowMotionDelay = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = levelSlowMotionDelay;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
